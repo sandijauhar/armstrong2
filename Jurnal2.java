46848ed10012c430e8c64c5b986feb9fc40c37e0
sandijauhar <sandijete42@gmail.com>
2015-04-10 23:10:03 +0700

    commit3

---
+++ Jurnal2.java
@@ -0,0 +1,19 @@
+
+
+package jurnal2;
+
+public class Jurnal2 {
+
+    
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        
+        for (int i = 0; i < b; i++) {
+            System.out.println(""+1);
+            
+        }
+        
+    }
+    
+}
---
+++ jurnal2/src/jurnal2/Jurnal2.java
@@ -0,0 +1,18 @@
+
+
+package jurnal2;
+
+public class Jurnal2 {
+
+    
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        
+        for (int i = 0; i < args.length; i++) {
+            String string = args[i];
+            
+        }
+    }
+    
+}
