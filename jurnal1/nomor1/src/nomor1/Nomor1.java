ce5f81e060a3a4a414dd032197ba30282923bc39
sandijauhar <sandijete42@gmail.com>
2015-04-10 22:33:02 +0700

    Commit

---
+++ jurnal1/nomor1/src/nomor1/Nomor1.java
@@ -0,0 +1,28 @@
+
+package nomor1;
+
+import java.util.Scanner;
+
+
+public class Nomor1 {
+
+   
+    public static void main(String[] args) {
+        int a=0;
+        int n=0;
+        
+        System.out.println("masukan angka : ");
+        Scanner input=new Scanner(System.in);
+        n=input.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+         i=i+a;
+         a++;
+            
+            
+        }
+        
+    }
+    
+}
